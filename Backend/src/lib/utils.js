61479888d6c1073f1b679f13a1936a430c5e9f18
Roman Chhatkuli <romanchhatkuli09@gmail.com>
2024-12-19 19:32:39 +0545

    update

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export default function generateToken(userId, res){
 
     res.cookie("jwt",token,{
         maxAge: 7*24*60*60*1000,
-        httpOnly: true,
+        // httpOnly: true,
         // sameSite: "None",
         // secure: process.env.NODE_ENV === "production"
     })
